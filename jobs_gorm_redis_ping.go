f56537c5f3ac0d1dc8051c9f8d92dfedf9340b6a
李光春 <gc@dtapp.net>
2022-08-12 11:11:45 +0800

    - 优化心跳

--- jobs_gorm_redis_ping.go
+++ jobs_gorm_redis_ping.go
@@ -2,16 +2,17 @@ package gojobs
 
 import (
 	"context"
+	"github.com/robfig/cron/v3"
 	"log"
 )
 
 // Ping 心跳
-func (j *JobsGorm) Ping(ctx context.Context) error {
-	result, err := j.redisClient.Set(ctx, j.config.cornKeyIp, j.config.outsideIp, 5).Result()
-
-	if j.config.debug == true {
-		log.Println("gojobs.Ping", j.config.cornKeyIp, j.config.outsideIp, result, err)
-	}
-
-	return err
+func (j *JobsGorm) Ping(ctx context.Context) {
+	c := cron.New(cron.WithSeconds())
+	_, _ = c.AddFunc(GetSeconds(2).Spec(), func() {
+		result, err := j.redisClient.Set(ctx, j.config.cornKeyIp, j.config.outsideIp, 3).Result()
+		if j.config.debug == true {
+			log.Println("JOBS心跳", j.config.cornKeyIp, j.config.outsideIp, result, err)
+		}
+	})
 }
