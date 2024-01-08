fddbe9a47e8aa7de7ee49b38c35d75b6d51a1d36
dtapps <li.gc@icloud.com>
2024-01-08 21:36:12 +0800

    - update get

--- get.go
+++ get.go
@@ -1,6 +1,21 @@
 package gojobs
 
-// GetCurrentIp 获取当前ip
+import (
+	"github.com/redis/go-redis/v9"
+	"gorm.io/gorm"
+)
+
+// GetGormDb 获取数据库驱动
+func (c *Client) GetGormDb() *gorm.DB {
+	return c.gormClient
+}
+
+// GetRedisDb 获取缓存数据库驱动
+func (c *Client) GetRedisDb() *redis.Client {
+	return c.cache.redisClient
+}
+
+// GetCurrentIp 获取当前IP
 func (c *Client) GetCurrentIp() string {
 	return c.config.systemOutsideIp
 }
